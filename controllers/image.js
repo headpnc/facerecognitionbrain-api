ddaaf594a2b8e6b462a9cf82c66899a3e4d45bf7
HeadPnC <boskepnc@gmail.com>
2021-09-21 00:14:12 +0200

    update

---
+++ controllers/image.js
@@ -0,0 +1,29 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '373d0d3eb731491096a33dbac6f226d2'
+});
+
+const handleApiCall = (req, res) => {
+	app.models.predict('f76196b43bbd45c99b4f3cd8e8b40a8a', req.body.input)
+	.then(data => {
+		res.json(data)
+	})
+	.catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	  .increment('entries', 1)
+	  .returning('entries')
+	  .then(entries =>{
+	  	res.json(entries[0]);
+	  })
+	  .catch(err => res.status(400).json('Unable to get entries'))
+}
+
+module.exports = {
+	handleImage,
+	handleApiCall
+}
